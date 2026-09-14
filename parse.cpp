520bd60f006cdaae1f0cb475d862d3e3a831a979
Austin Devore <ard78@students.uwf.edu>
2026-09-13 20:27:26 -0500

    Fixed the parser tokenizing

--- parse.cpp
+++ parse.cpp
@@ -8,14 +8,17 @@ void parse(char* line, Param& p){
     char* token = strtok(line, " \n\t");
 
     while(token != nullptr){
+        char* nextToken = strtok(nullptr, " \n\t");
         if(token[0] == '<'){
             p.setInputRedirect(token+1);
-        }else if(token[0=='>']){
+        }else if(token[0]=='>'){
             p.setOutputRedirect(token+1);
-        }else if(token[]){
-
+        }else if(token[0]=='&' && nextToken == nullptr){
+            p.setBackground(1);
+        }else{
+            p.addArgument(token);
         }
-        token = strtok(nullptr, " \n\t");
+        token = nextToken;
     }
     
 }
